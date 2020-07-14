bd15d86a0641812c0941e682c04ecc1f1354bd55
周虎 <zhouhu@leyoujia.com>
2020-07-14 11:55:05 +0800

    单侧

--- src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
@@ -1,14 +1,51 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.ApplicationTests;
+import com.example.demo.enity.UserVo;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.IUserSerice;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 //启动Spring
 public class UserServiceImplTest extends ApplicationTests{
 
+/*
+    @MockBean
+    private IUserSerice userSericeMock;
+*/
+
+    @Autowired
+    private IUserSerice userSerice;
+
+    @Autowired
+    private UserMapper userMapper;
+/*
+    @Test
+    public void getTestInfoOnMock() {
+        //Mockito 就是只测业务逻辑而不会访问数据库，里面直接指定mapper返回的数据
+        UserVo userVo = new UserVo();
+        userVo.setAge(22);
+        userVo.setId(1);
+        userVo.setName("zh");
+        List<UserVo> list = new ArrayList<>();
+        list.add(userVo);
+        Mockito.when(userMapper.getTestInfo()).thenReturn(list);
+        List<UserVo> testInfo = userSericeMock.getTestInfo();
+        assertTrue(testInfo.size()>=1);
+    }*/
+
+
     @Test
     public void getTestInfo() {
-        System.out.println(1111);
+        List<UserVo> testInfo = userSerice.getTestInfo();
+        assertTrue(testInfo.size()>=1);
     }
 
 }
