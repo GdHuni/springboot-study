8c7f4d39a40953ee7b69d06b97f803516ac1e678
周虎 <zhouhu@leyoujia.com>
2020-07-15 14:58:21 +0800

    添加controller层单测

--- src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
@@ -5,10 +5,8 @@ import com.example.demo.enity.UserVo;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.IUserSerice;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import java.util.ArrayList;
+
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
